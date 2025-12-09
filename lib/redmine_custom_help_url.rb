3f067ca6298c8307dc71852afb6a5f2aa0b4ca13
Bradley Shade <bshade7@gmail.com>
2025-12-09 10:20:23 -0600

    Removed unloadable legacy Rails code deprecated

--- lib/redmine_custom_help_url.rb
+++ lib/redmine_custom_help_url.rb
@@ -2,9 +2,6 @@ require_dependency 'redmine/info'
 
 # Create namespace module for override
 module RedmineCustomHelpUrl
-  # Make it work in development environment
-  unloadable
-  
   # TODO: needs a restart of Redmine after custom_help_url setting value change
   # Override core Redmine::Info.help_url, when
   # Setting.plugin_redmine_custom_help_url['custom_help_url'] contains
